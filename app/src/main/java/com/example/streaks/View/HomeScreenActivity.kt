c67bad786b093a67c30e8d2541e120f242f86dbd
Joseph Sanusi <josephsanusi4@gmail.com>
2025-07-24 13:28:47 +0100

    ViewModel initialised in UI

--- app/src/main/java/com/example/streaks/View/HomeScreenActivity.kt
+++ app/src/main/java/com/example/streaks/View/HomeScreenActivity.kt
@@ -2,22 +2,30 @@ package com.example.streaks.View
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.LocalActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Home
@@ -35,6 +43,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -42,6 +51,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -52,7 +62,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.view.WindowCompat
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.streaks.Model.StreakModel
 import com.example.streaks.R
+import com.example.streaks.ViewModel.StreakViewModel
 import com.example.streaks.ui.theme.StreaksTheme
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import kotlinx.coroutines.coroutineScope
@@ -80,7 +93,6 @@ class HomeScreenActivity : ComponentActivity() {
 fun scaffoldScreen(){
 
     val systemUiController = rememberSystemUiController()
-
         systemUiController.setSystemBarsColor(
             Color.Transparent ,
             darkIcons = true
@@ -213,19 +225,106 @@ fun scaffoldScreen(){
 fun HomeScreen(paddingValues: PaddingValues)
 {
 
-    Column(modifier = Modifier.padding(paddingValues)
-        .fillMaxSize()
-        .background(Color.White)
-        , verticalArrangement = Arrangement.Center ,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        Text("No Streak created yet!"
-            , fontSize = 20.sp
-            , fontWeight = FontWeight.SemiBold
-            , color = Color.Blue
-        )
+val viewModel : StreakViewModel = hiltViewModel()
+val streaks by viewModel.streaks.collectAsState()
+
+
+    if (streaks.isEmpty()) {
+        Column(
+            modifier = Modifier.padding(paddingValues)
+                .fillMaxSize()
+                .background(Color.White), verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                "No Streak created yet!",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.SemiBold,
+                color = Color.Blue
+            )
+        }
     }
+
+        else{
+            Column(
+                modifier = Modifier.padding(paddingValues)
+                    .fillMaxSize()
+                    .background(Color.White) ,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                LazyColumn {
+                    items(streaks){streak->
+
+
+                    }
+                }
+            }
+    }
+
 }
+
+
+@Composable
+fun Streaks(streak : StreakModel){
+
+    Log.d("OMO", "Loaded color value: ${streak.color}")
+
+    Surface(color = Color.White ,
+        shape = RoundedCornerShape(10.dp),
+        shadowElevation = 1.dp,
+        border = BorderStroke(1.dp , color = Color.LightGray),
+        modifier = Modifier.fillMaxWidth()
+            .padding(horizontal = 20.dp)
+            .padding(bottom = 10.dp)
+        , tonalElevation = 8.dp
+
+    ) {
+        Box(
+            modifier = Modifier.fillMaxWidth()
+                .padding(10.dp)
+        ) {
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Column {
+                    Text(
+                        streak.streakName,
+                        Modifier.padding(bottom = 12.dp),
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 25.sp
+                    )
+
+//                    Text(
+//                        viewModel.nextCount(streak),
+//                        Modifier.padding(bottom = 12.dp),
+//                        fontSize = 15.sp,
+//                        color = Color.Gray
+//                    )
+                }
+
+//                Box(
+//                    modifier = Modifier.size(55.dp)
+//                        .clip(shape = CircleShape)
+//                        .border(width = 3.dp, color = streak.color , shape = CircleShape),
+//                    Alignment.Center
+//                ) {
+//                    Text(
+//                       " viewModel.calculateStreakCount(streak).toString()",
+//                        fontWeight = FontWeight.Bold,
+//                        fontSize = 22.sp,
+//                        color = streak.color
+//                    )
+                }
+            }
+        }
+    }
+
+
+
+
 
 @Preview
 @Composable
